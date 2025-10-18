4fd063d882e9c273b7e408db8ae13d4a259f930d
GowthamL <45562756+Gowtham-L@users.noreply.github.com>
2025-10-18 06:54:21 +0530

    IndentationError fix in function

--- raven/utils.py
+++ raven/utils.py
@@ -215,9 +215,9 @@ def clear_thread_reply_count_cache(thread_id: str):
 	frappe.cache().hdel("raven:thread_reply_count", thread_id)
 
 def after_request(response):
-	if "X-Frame-Options" in response.headers:
-		del response.headers["X-Frame-Options"]
-	
-	response.headers["Content-Security-Policy"] = "frame-ancestors *"
+    if 'X-Frame-Options' in response.headers:
+        del response.headers['X-Frame-Options']
+
+    response.headers['Content-Security-Policy'] = 'frame-ancestors *'
 
     return response
